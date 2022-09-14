ee8767b615d369f0d9372b938c7e5c19d7866bf5
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-14 20:51:07 +0500

    tests

--- src/first.rs
+++ src/first.rs
@@ -37,3 +37,23 @@ impl List {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basic(){
+        let mut list = List::new();
+        assert_eq!(list.pop(),None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(),Some(3));
+        assert_eq!(list.pop(),Some(2));
+        assert_eq!(list.pop(),Some(1));
+        assert_eq!(list.pop(),None);
+
+    }
+}
