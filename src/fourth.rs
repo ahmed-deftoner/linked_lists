a2e6d53a03e5a6ce0e567a0d61c72add514da9ea
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-18 20:38:22 +0500

    structure

---
+++ src/fourth.rs
@@ -0,0 +1,14 @@
+use std::{rc::Rc, cell::RefCell};
+
+pub struct List<T> {
+    head: Link<T>,
+    tail: Link<T>
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    prev: Link<T>,
+    next: Link<T>
+}
