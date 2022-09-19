723f0a7e3eec3d435a13d1051b13eb8a6aa91bd3
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-19 14:42:16 +0500

    into iter

--- src/fourth.rs
+++ src/fourth.rs
@@ -13,6 +13,8 @@ struct Node<T> {
     next: Link<T>
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> Node<T> {
     pub fn new(elem: T) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Node{
@@ -110,10 +112,21 @@ impl<T> List<T> {
         })
     }
     
-    pub fn peek_back_mutt(&mut self) -> Option<RefMut<T>> {
+    pub fn peek_back_mut(&mut self) -> Option<RefMut<T>> {
         self.tail.as_ref().map(|node| {
             RefMut::map(node.borrow_mut(), |node| &mut node.elem)
         })
+    }
+
+    pub fn into_iter(self) -> IntoIter<T>{
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()   
     }
 }
 
@@ -148,14 +161,48 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+
+        // ---- back -----
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_back(), None);
+
+        // Populate list
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_back(4);
+        list.push_back(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_back(), Some(5));
+        assert_eq!(list.pop_back(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 
     #[test]
     fn peek() {
         let mut list = List::new();
         assert!(list.peek_front().is_none());
+        assert!(list.peek_back().is_none());
+        assert!(list.peek_front_mut().is_none());
+        assert!(list.peek_back_mut().is_none());
+
         list.push_front(1); list.push_front(2); list.push_front(3);
 
         assert_eq!(&*list.peek_front().unwrap(), &3);
+        assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
+        assert_eq!(&*list.peek_back().unwrap(), &1);
+        assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
     }
+
 }
